ae1b1a1165105dc90b24e438beda05dac46a6d32
Victor Albertsson <victor.albertsson@live.se>
2020-11-25 13:55:49 +0100

    Improved dependency form generation and updated README.md.

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # make-project
-***Easy and modern Common LISP project setup***
+***Easy, modern and lightweight Common LISP project setup***
 ## Getting started
-- Make sure that you have `quicklisp` and `trivial-types` installed
+- Make sure that you have `quicklisp` installed
 - Clone this repository into some `ql:*local-project-directories*`
 - Load the library with `(ql:quickload :make-project)`
-- Create your new project with `(prj:new :insert-awesome-project-name-here)`
+- Create your new project with `(prj:new "insert-awesome-project-name-here")`
+- If you want to initialize with dependencies: `(prj:new "project-name" "dep-1" "dep-2")`
 ## Dependencies
-- ASDF
+- ASDF (included in most implementations)
 - quicklisp
 - trivial-types
+## Notes
+- `make-project` adds `serapeum` and `alexandria` to all `:use` forms
+- `make-project` currently is not on `quicklisp`, however this might change
 ## API
 - `new`
-- `make-asd`
+- `make-asdf-system`
 - `make-packages`
--- src/main.lisp
+++ src/main.lisp
@@ -1,52 +1,61 @@
 (defpackage :make-project/src/main
   (:use :common-lisp)
   (:import-from :trivial-types #:proper-list)
-  (:export #:new #:make-asd #:make-packages))
+  (:export #:new #:make-asdf-system #:make-packages))
 
 (in-package :make-project/src/main)
 
-(declaim (optimize (speed 2)
-		   (safety 1)))
+(declaim (optimize (speed 2) (safety 1)))
 
 (declaim (ftype (function (string &optional (proper-list string))) new))
-(defun new (name &optional deps)
-  (make-asd name)
+(defun new (name &rest deps)
+  (make-asdf-system name)
   (make-packages name deps))
 
-(declaim (ftype (function (string &optional string)) make-asd))
-(defun make-asd (name &optional (subdir "src"))
+(declaim (ftype (function (string &optional string)) make-asdf-system))
+(defun make-asdf-system (name &optional (subdir "src"))
   (let ((path (format nil "~a/~a"
-		      (first ql:*local-project-directories*)
-		      (concatenate 'string name "/" name ".asd"))))
+                      (first ql:*local-project-directories*)
+                      (concatenate 'string name "/" name ".asd"))))
+
     (declare (type (string) path))
     (with-open-file (file (ensure-directories-exist path)
-			  :direction :output
-			  :if-exists :supersede)
-      (if (not (null file))
-	  (format file "(defsystem :~a~%  :class :package-inferred-system~%  :depends-on (\"~:*~a/~a/all\"))~%"
-		  name
-		  subdir)))))
+                          :direction :output
+                          :if-does-not-exist :create
+                          :if-exists :supersede)
+      (when (not (null file))
+            (format file "(defsystem :~a~%  :class :package-inferred-system~%  :depends-on (\"~:*~a/~a/all\")~%"
+                  name
+                  subdir)))))
 
 (declaim (ftype (function (string (proper-list string) &optional string)) make-packages))
 (defun make-packages (name deps &optional (subdir "src"))
   (let ((all-file (format nil "~a/~a"
-			  (first ql:*local-project-directories*)
-			  (concatenate 'string name "/" subdir "/all.lisp")))
-	(main-file (format nil "~a/~a"
-			   (first ql:*local-project-directories*)
-			   (concatenate 'string name "/" subdir "/main.lisp"))))
+                          (first ql:*local-project-directories*)
+                          (concatenate 'string name "/" subdir "/all.lisp")))
+        (main-file (format nil "~a/~a"
+                           (first ql:*local-project-directories*)
+                           (concatenate 'string name "/" subdir "/main.lisp"))))
     (declare (type (string) all-file))
+    (declare (type (string) main-file))
     (with-open-file (file (ensure-directories-exist all-file)
-			  :direction :output
-			  :if-exists :supersede)
-      (format file "(defpackage :~a~%  (:use :common-lisp)~%  (:nicknames :~a)~%  (:import-from :~a))~%"
-	      (concatenate 'string name "/" subdir "/all")
-	      name
-	      (concatenate 'string name "/" subdir "/main")))
+                          :direction :output
+                          :if-does-not-exist :create
+                          :if-exists :supersede)
+      (format file "(defpackage :~a~%  (:use :common-lisp :serapeum :alexandria)~%  (:nicknames :~a)~%  (:import-from :~a))~%"
+              (concatenate 'string name "/" subdir "/all")
+              name
+              (concatenate 'string name "/" subdir "/main")))
     (with-open-file (file (ensure-directories-exist main-file)
-			  :direction :output
-			  :if-exists :supersede)
-      (format file "(defpackage :~a~%  (:use :common-lisp)~{~&  (:import-from :~a)~})~%~%(in-package :~a)~%"
-	      (concatenate 'string name "/" subdir "/main")
-	      deps
-	      (concatenate 'string name "/" subdir "/main")))))
+                          :direction :output
+                          :if-does-not-exist :create
+                          :if-exists :supersede)
+      ;; TODO If `deps` is empty don't include the `:import-from` line
+      (if (null deps)
+          (format file "(defpackage :~a~%  (:use :common-lisp :serapeum :alexandria))~%~%(in-package :~a)~%"
+                  (concatenate 'string name "/" subdir "/main")
+                  (concatenate 'string name "/" subdir "/main"))
+          (format file "(defpackage :~a~%  (:use :common-lisp :serapeum :alexandria)~{~&  (:import-from :~a)~})~%~%(in-package :~a)~%"
+                  (concatenate 'string name "/" subdir "/main")
+                  deps
+                  (concatenate 'string name "/" subdir "/main"))))))
