182c29f44e4f660ae1b6c0b74711b183a1f41514
Victor Albertsson <victor.albertsson@live.se>
2020-11-25 16:54:39 +0100

    Use keywords instead of strings in prj:new, proper utilization of Common LISP pathname facilities.

--- src/main.lisp
+++ src/main.lisp
@@ -5,57 +5,70 @@
 
 (in-package :make-project/src/main)
 
-(declaim (optimize (speed 2) (safety 1)))
+(declaim (optimize (speed 3)))
 
-(declaim (ftype (function (string &optional (proper-list string))) new))
+(declaim (ftype (function (keyword &optional (proper-list keyword))) new))
 (defun new (name &rest deps)
-  (make-asdf-system name)
-  (make-packages name deps))
+  (make-asdf-system (string name))
+  (make-packages (string name) deps))
 
 (declaim (ftype (function (string &optional string)) make-asdf-system))
 (defun make-asdf-system (name &optional (subdir "src"))
-  (let ((path (format nil "~a/~a"
-                      (first ql:*local-project-directories*)
-                      (concatenate 'string name "/" name ".asd"))))
-
-    (declare (type (string) path))
+  (let* ((name* (string-downcase name))
+         (path (make-pathname
+                 :directory
+                 (append (pathname-directory  (first ql:*local-project-directories*))
+                         (list (pathname-name (first ql:*local-project-directories*)))
+                         (list name*))
+                 :name name*
+                 :type "asd")))
+    (declare (type (pathname) path))
     (with-open-file (file (ensure-directories-exist path)
                           :direction :output
                           :if-does-not-exist :create
                           :if-exists :supersede)
       (when (not (null file))
             (format file "(defsystem :~a~%  :class :package-inferred-system~%  :depends-on (\"~:*~a/~a/all\")~%"
-                  name
+                  name*
                   subdir)))))
 
 (declaim (ftype (function (string (proper-list string) &optional string)) make-packages))
 (defun make-packages (name deps &optional (subdir "src"))
-  (let ((all-file (format nil "~a/~a"
-                          (first ql:*local-project-directories*)
-                          (concatenate 'string name "/" subdir "/all.lisp")))
-        (main-file (format nil "~a/~a"
-                           (first ql:*local-project-directories*)
-                           (concatenate 'string name "/" subdir "/main.lisp"))))
-    (declare (type (string) all-file))
-    (declare (type (string) main-file))
+  (let* ((name* (string-downcase name))
+         (deps* (mapcar #'string-downcase deps))
+         (all-file (make-pathname
+                     :directory
+                     (append (pathname-directory  (first ql:*local-project-directories*))
+                             (list (pathname-name (first ql:*local-project-directories*)))
+                             (list name* subdir))
+                     :name "all"
+                     :type "lisp"))
+         (main-file (make-pathname
+                      :directory
+                      (append (pathname-directory  (first ql:*local-project-directories*))
+                              (list (pathname-name (first ql:*local-project-directories*)))
+                              (list name* subdir))
+                      :name "main"
+                      :type "lisp")))
+    (declare (type (pathname) all-file))
+    (declare (type (pathname) main-file))
     (with-open-file (file (ensure-directories-exist all-file)
                           :direction :output
                           :if-does-not-exist :create
                           :if-exists :supersede)
       (format file "(defpackage :~a~%  (:use :common-lisp :serapeum :alexandria)~%  (:nicknames :~a)~%  (:import-from :~a))~%"
-              (concatenate 'string name "/" subdir "/all")
-              name
-              (concatenate 'string name "/" subdir "/main")))
+              (concatenate 'string name* "/" subdir "/" "all")
+              name*
+              (concatenate 'string name* "/" subdir "/" "main")))
     (with-open-file (file (ensure-directories-exist main-file)
                           :direction :output
                           :if-does-not-exist :create
                           :if-exists :supersede)
-      ;; TODO If `deps` is empty don't include the `:import-from` line
-      (if (null deps)
+      (if (null deps*)
           (format file "(defpackage :~a~%  (:use :common-lisp :serapeum :alexandria))~%~%(in-package :~a)~%"
-                  (concatenate 'string name "/" subdir "/main")
-                  (concatenate 'string name "/" subdir "/main"))
+                  (concatenate 'string name* "/" subdir "/" "main")
+                  (concatenate 'string name* "/" subdir "/" "main"))
           (format file "(defpackage :~a~%  (:use :common-lisp :serapeum :alexandria)~{~&  (:import-from :~a)~})~%~%(in-package :~a)~%"
-                  (concatenate 'string name "/" subdir "/main")
-                  deps
-                  (concatenate 'string name "/" subdir "/main"))))))
+                  (concatenate 'string name* "/" subdir "/" "main")
+                  deps*
+                  (concatenate 'string name* "/" subdir "/" "main"))))))
